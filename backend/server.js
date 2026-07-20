1dc67338e6e3cee75da5690067a2a0e8d50df167
murarie <murarimishra1611@gmail.com>
2026-07-20 18:50:34 +0530

    Create register API and test with Postman

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const db = require("./config/db");
 
 const app = express();
+
 const PORT = 3000;
 
 app.use(cors());
@@ -20,6 +21,58 @@ app.get("/", (req, res) => {
         res.json(results);
 
     });
+
+});
+
+app.post("/register", (req, res) => {
+
+    const {
+
+        full_name,
+
+        roll_number,
+
+        email,
+
+        password
+
+    } = req.body;
+
+    const sql = `
+        INSERT INTO students
+        (full_name, roll_number, email, password)
+        VALUES (?, ?, ?, ?)
+    `;
+
+    db.query(
+
+        sql,
+
+        [full_name, roll_number, email, password],
+
+        (error, result) => {
+
+            if (error) {
+
+                console.log(error);
+
+                return res.status(500).json({
+
+                    message: "Registration Failed"
+
+                });
+
+            }
+
+            res.json({
+
+                message: "Registration Successful"
+
+            });
+
+        }
+
+    );
 
 });
 
